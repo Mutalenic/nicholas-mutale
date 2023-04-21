73f5e05d9cca4019838a159b47a4ccad9bc00e3b
Nicholas Mutale <nicomutale@gmail.com>
2023-04-21 19:44:15 +0200

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -12,24 +12,19 @@ const About = () => {
             About me
           </p>
           <p className="py-6">
-            Hello!My name is Nicholas Mutale and I enjoy creating web applications.I
-            have always been curious to know how web apps on the internet
-            work and when MOOCs were introduced back in 2008, I was excited to
-            increase my knowledge of software development and enrolled in a few
-            courses.
+            Hello there! My name is Nicholas Mutale, and I am passionate about creating web applications. From a young age, I have always been curious about how web apps work, and my curiosity led me to enroll in some MOOCs when they were introduced in 2008. These courses helped me increase my knowledge of software development, and I have been hooked ever since.
           </p>
           <p className="py-6">
-            Fast-forward 2022, I enrolled at Microverse, a remote software
-            development program that uses pair programming and project building
-            to teach development. I spent more than 2000+ hours learning web
-            development.
+            In 2022, I decided to take my skills to the next level and enrolled in Microverse, a remote software development program that emphasizes pair programming and project building. Over the course of 2000+ hours, I have gained a deep understanding of web development.
           </p>
           <p className="py-6">
-            These are some of the technologies I have been working with
-            recently; HTML, CSS, JavaScript, React, Redux, Ruby, Ruby on rails
+            Among the technologies I have been working with recently are HTML, CSS, and JavaScript - the foundational languages of the web - as well as React and Redux, which allow for the creation of powerful, data-driven front-end applications. On the back end, I have also been working with Ruby and Ruby on Rails, which provide an elegant and efficient framework for building robust, scalable web applications.
           </p>
+    <p className="py-6">I am always eager to take on new challenges and contribute to exciting projects. With my vast experience and expertise, I am confident in my ability to create high-quality web applications that are both functional and visually appealing.</p>
           <p className="py-8 pt-8 text-gray-600 underline cursor-pointer">
-            <Link href="/#work">Checkout some of my latest projects</Link>
+            <Link href="/#work">
+    
+    Checkout some of my latest projects</Link>
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
