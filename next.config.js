d044ca608950065d8242884fe259dc423800d21e
mutalenic <nicomutale@gmail.com>
2025-04-21 19:28:41 +0200

    remove empty navbar.jsx

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  // Add webpack configuration to optimize Fast Refresh
+  webpack: (config, { isServer }) => {
+    // Keep Fast Refresh enabled in development
+    if (!isServer) {
+      config.optimization.runtimeChunk = 'single';
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
